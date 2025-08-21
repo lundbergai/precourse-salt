25698ef2055765ce24e5c5d5a92ab3578f941dee
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-21 11:43:40 +0200

    replace hello world with greeting

--- my-first-dotnet-webserver/Program.cs
+++ my-first-dotnet-webserver/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/greet/{name}", (string name) => $"Welcome to SALT, {name}!");
 
 app.Run();
