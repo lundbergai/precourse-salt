0597f93f0e06388d59d31934348f5b6f1efed633
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-21 11:59:12 +0200

    feat: start date calculation in Greet

--- first-dotnet-app/Program.cs
+++ first-dotnet-app/Program.cs
@@ -3,11 +3,22 @@ var name = Console.ReadLine()!;
 Console.Write("When does that course start? ");
 var startDate = Console.ReadLine()!;
 
-var greeting = Greet(name);
+var greeting = Greet(name, startDate);
 
 Console.WriteLine(greeting);
 
-static string Greet(string namePassedIn)
+static string Greet(string namePassedIn, string startDate)
 {
-  return $"Welcome to SALT, {namePassedIn}";
+  try
+  {
+    var startDateParsed = DateTime.Parse(startDate);
+    var today = DateTime.Now;
+    var daysUntilStart = (startDateParsed - today).Days;
+
+    return $"Welcome to SALT, {namePassedIn}. \nToday it is {daysUntilStart} days left until the course starts";
+  }
+  catch (Exception e)
+  {
+    return $"Error: {e.Message}";
+  }
 }
