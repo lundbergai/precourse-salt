965d9a0c469c217454708b07eece920b34149086
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-21 11:15:54 +0200

    add initial first-dotnet-app assets

---
+++ first-dotnet-app/Program.cs
@@ -0,0 +1,11 @@
+Console.Write("What is your name? ");
+var name = Console.ReadLine()!;
+
+var greeting = Greet(name);
+
+Console.WriteLine(greeting);
+
+﻿static string Greet(string namePassedIn)
+{
+  return "Welcome to SALT, ";
+}
---
+++ first-dotnet-app/ReadMe.md
@@ -0,0 +1,3 @@
+# First .NET Application for the SALT PreCourse Material
+
+To be used while reading [the .NET PreCourse intro](https://docs.salt.dev/dotnet/language-intro/)
