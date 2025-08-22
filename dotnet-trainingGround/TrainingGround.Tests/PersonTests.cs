a16bee30bc36fbddd9f6c6dbefc8f2208e033fbe
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-22 10:38:43 +0200

    create new test with parameter

--- dotnet-trainingGround/TrainingGround.Tests/PersonTests.cs
+++ dotnet-trainingGround/TrainingGround.Tests/PersonTests.cs
@@ -4,11 +4,25 @@ public class PersonTests
 	public void create_person_using_parameterless_constructor()
 	{
 		// arrange
-		var p = new Person();
 
 		// act
+		var p = new Person();
 
 		// assert
 		Assert.NotNull(p);
+	}
+
+	[Fact]
+	public void create_person_using_constructor_with_name()
+	{
+		// arrange
+		var name = "Marcus";
+
+		// act
+		var p = new Person(name);
+
+		// assert
+		Assert.NotNull(p);
+		Assert.Equal(name, p.Name);
 	}
 }
