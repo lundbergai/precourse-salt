2dfef285ba15a51c57dcf8725565a056fdb03b92
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-22 10:38:10 +0200

    add parameterless and parameter constructor

--- dotnet-trainingGround/TrainingGround/Person.cs
+++ dotnet-trainingGround/TrainingGround/Person.cs
@@ -3,4 +3,10 @@ public class Person
 	public string Name;
 	public int BirthYear;
 	public double LengthInMeters;
+
+	public Person() { }
+	public Person(string name)
+	{
+		this.Name = name;
+	}
 }
