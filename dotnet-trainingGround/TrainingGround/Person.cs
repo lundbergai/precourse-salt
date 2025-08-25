11291788092ab367ae39e1848d4539140348844f
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-25 13:59:46 +0200

    overload Person constructor with name, birthYear

--- dotnet-trainingGround/TrainingGround/Person.cs
+++ dotnet-trainingGround/TrainingGround/Person.cs
@@ -1,13 +1,20 @@
 public class Person
 {
 	public string? Name { get; private set; }
-	public int BirthYear { get; set; }
+	public int BirthYear { get; private set; }
 	public double LengthInMeters { get; set; }
 
 	public Person() { }
+
 	public Person(string name)
 	{
 		this.Name = name;
+	}
+
+	public Person(string name, int birthYear)
+	{
+		this.Name = name;
+		this.BirthYear = birthYear;
 	}
 
 	public int GetAge(int currentYear)
