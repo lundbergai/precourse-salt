9261039b38d0fe5a5b94378254e2cebef4d2a5d7
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-25 14:29:06 +0200

    add type test

--- dotnet-trainingGround/TrainingGround.Tests/TypesTests.cs
+++ dotnet-trainingGround/TrainingGround.Tests/TypesTests.cs
@@ -32,4 +32,19 @@ public class TypesTests
 		Assert.Equal(1.96, bLengthInMeters);
 		Assert.Equal(1.95, aLengthInMeters);
 	}
+
+	[Fact]
+	public void getting_age_from_person()
+	{
+		// arrange
+		var p = new Person("Viktor", 1972);
+
+		// act
+		var age = p.GetAge(2022);
+
+		// assert
+		Assert.Equal(50, age);
+		Assert.IsType(typeof(Person), p);
+		Assert.IsType(typeof(int), age);
+	}
 }
