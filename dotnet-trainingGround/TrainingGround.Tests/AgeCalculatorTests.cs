c5a1d99afcfcd362c9dcee1f421dd543cc85079d
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-25 14:14:15 +0200

    make GetAge static method

--- dotnet-trainingGround/TrainingGround.Tests/AgeCalculatorTests.cs
+++ dotnet-trainingGround/TrainingGround.Tests/AgeCalculatorTests.cs
@@ -2,18 +2,11 @@ namespace TrainingGround.Tests;
 
 public class AgeCalculatorTests
 {
-    private readonly AgeCalculator _calculator;
-
-    public AgeCalculatorTests()
-    {
-        _calculator = new AgeCalculator();
-    }
-
     [Fact]
     public void someone_born_1972_is_50_in_2022()
     {
         // act
-        var age = _calculator.GetAge(1972, 2022);
+        var age = AgeCalculator.GetAge(1972, 2022);
 
         // assert
         Assert.Equal(50, age);
@@ -23,7 +16,7 @@ public class AgeCalculatorTests
     public void someone_born_2022_is_0_in_2022()
     {
         // act
-        var age = _calculator.GetAge(2022, 2022);
+        var age = AgeCalculator.GetAge(2022, 2022);
 
         // assert
         Assert.Equal(0, age);
