1def7283f72370b4d16da7207623b44ed31dc1b2
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-25 14:37:43 +0200

    create Employee entity

--- dotnet-trainingGround/TrainingGround/Employee.cs
+++ dotnet-trainingGround/TrainingGround/Employee.cs
@@ -1,6 +1,13 @@
-namespace TrainingGround
+namespace TrainingGround;
+
+public class Employee : Person
 {
-	public class Employee : Person
+	public string EmployeeId { get; set; }
+
+	public Employee() { }
+
+	public Employee(string name, string employeeId) : base(name)
 	{
+		EmployeeId = employeeId;
 	}
 }
