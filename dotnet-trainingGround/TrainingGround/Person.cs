2424b1b468e1245e1a94680e6c7721aa202950b2
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-25 13:44:23 +0200

    add instance method

--- dotnet-trainingGround/TrainingGround/Person.cs
+++ dotnet-trainingGround/TrainingGround/Person.cs
@@ -1,12 +1,18 @@
 public class Person
 {
 	public string? Name { get; private set; }
-	public int? BirthYear { get; private set; }
-	public double? LengthInMeters { get; private set; }
+	public int BirthYear { get; set; }
+	public double LengthInMeters { get; set; }
 
 	public Person() { }
 	public Person(string name)
 	{
 		this.Name = name;
 	}
+
+	public int GetAge(int currentYear)
+	{
+		return currentYear - this.BirthYear;
+	}
+
 }
