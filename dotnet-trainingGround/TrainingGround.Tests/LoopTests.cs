d85fa935df2c5fa83bf0cfc8b3197c568caa82da
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-26 10:50:58 +0200

    add for-loop test

--- dotnet-trainingGround/TrainingGround.Tests/LoopTests.cs
+++ dotnet-trainingGround/TrainingGround.Tests/LoopTests.cs
@@ -21,4 +21,23 @@ public class LoopTests
 			i = i + 1;
 		}
 	}
+
+	[Fact]
+	public void for_loop()
+	{
+		// arrange
+		var ints = new int[] { 1, 2, 3, 4, 5 };
+
+		// act
+		for (var i = 0; i < ints.Length; i++)
+		{
+			var currentValueInTheLoop = ints[i];
+
+			Console.WriteLine($"i is now '{i}'");
+			Console.WriteLine($"currentValueInTheLoop is now '{currentValueInTheLoop}'");
+
+			// assert
+			Assert.Equal(i + 1, currentValueInTheLoop);
+		}
+	}
 }
