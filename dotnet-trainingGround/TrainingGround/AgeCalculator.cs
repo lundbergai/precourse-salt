68da90828ed1016694cc9afff180e2e051710d22
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-26 09:43:48 +0200

    create enum and getagecategory

--- dotnet-trainingGround/TrainingGround/AgeCalculator.cs
+++ dotnet-trainingGround/TrainingGround/AgeCalculator.cs
@@ -2,8 +2,15 @@
 
 public class AgeCalculator
 {
+	public enum AgeCategory { Kid }
+
 	public static int GetAge(int birthYear, int currentYear)
 	{
 		return currentYear - birthYear;
+	}
+
+	public static AgeCategory GetAgeCategory(Person person, int currentYear)
+	{
+		return AgeCategory.Kid;
 	}
 }
