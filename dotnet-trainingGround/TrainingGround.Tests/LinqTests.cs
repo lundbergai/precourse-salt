de9939ce38a8fa2a05b7b31331dc41fe1a96075d
Viktor Lundberg <vikkolundberg@gmail.com>
2025-08-28 13:25:20 +0200

    add test with linq Where

--- dotnet-trainingGround/TrainingGround.Tests/LinqTests.cs
+++ dotnet-trainingGround/TrainingGround.Tests/LinqTests.cs
@@ -53,4 +53,36 @@ public class LinqTests
 		var expectedNumber = 53;
 		Assert.Equal(expectedNumber, filteredNumber);
 	}
+
+	[Fact]
+	public void linq_to_check_if_any_matches()
+	{
+		// arrange
+		var numbers = new List<int> { 1, 53, 2, 62, 2, 12, 17, 15, 16 };
+
+		// act
+		var hasNumber = numbers.Any(n => n > 100);
+
+		// assert
+		Assert.False(hasNumber);
+	}
+
+	[Fact]
+	public void linq_to_filter_people_by_age()
+	{
+		// arrange
+		var people = new List<Person>{
+			new Person("Aaaron"),
+			new Person("Bea"),
+			new Person("Ceasar"),
+			new Person("Dave")
+		};
+
+		// act
+		var filteredPersons = people.Where(p => p.Name.Length > 4).ToList();
+
+		// assert
+		Assert.Equal(2, filteredPersons.Count);
+		Assert.Equal("Aaaron", filteredPersons[0].Name);
+	}
 }
