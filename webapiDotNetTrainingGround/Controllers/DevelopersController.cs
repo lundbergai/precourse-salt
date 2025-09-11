461b7e8e62ccabd85e4217d0a56f075fd2f49774
Viktor Lundberg <vikkolundberg@gmail.com>
2025-09-11 12:43:38 +0200

    create developers controller with GetDevelopers()

---
+++ webapiDotNetTrainingGround/Controllers/DevelopersController.cs
@@ -0,0 +1,26 @@
+using webapiDotNetTrainingGround.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace webapiDotNetTrainingGround.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DevelopersController : ControllerBase
+{
+	private readonly List<Developer> _db;
+
+	public DevelopersController()
+	{
+		_db = new();
+	}
+
+	[HttpGet]
+	public List<Developer> GetDevelopers()
+	{
+		return _db.Select(d => new Developer
+		{
+			Id = d.Id,
+			MyName = d.MyName,
+			Email = d.Email
+		}).ToList();
+	}
+}
