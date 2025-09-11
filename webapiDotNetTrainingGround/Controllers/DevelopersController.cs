97387c01c570fa128443226a2f8de45a08fc61e6
Viktor Lundberg <vikkolundberg@gmail.com>
2025-09-11 13:46:04 +0200

    create post method and make list static

--- webapiDotNetTrainingGround/Controllers/DevelopersController.cs
+++ webapiDotNetTrainingGround/Controllers/DevelopersController.cs
@@ -6,15 +6,11 @@ namespace webapiDotNetTrainingGround.Controllers;
 [Route("api/[controller]")]
 public class DevelopersController : ControllerBase
 {
-	private readonly List<Developer> _db;
-
-	public DevelopersController()
-	{
-		_db = new List<Developer>() {
+	private static int _nextId = 3;
+	private static List<Developer> _db = new List<Developer>() {
 			new Developer() { Id = 1, Name = "Marcus", Email = "marcus@salt.dev" },
 			new Developer() { Id = 2, Name = "Beatrice", Email = "bea@salt.dev" },
 		};
-	}
 
 	[HttpGet]
 	public List<Developer> GetDevelopers()
@@ -36,5 +32,14 @@ public class DevelopersController : ControllerBase
 			Name = d.Name,
 			Email = d.Email
 		}).FirstOrDefault();
+	}
+
+	[HttpPost]
+	public IActionResult CreateDeveloper(Developer newDev)
+	{
+		newDev.Id = _nextId++;
+		_db.Add(newDev);
+
+		return CreatedAtAction(nameof(GetDeveloperById), new { id = newDev.Id }, newDev);
 	}
 }
