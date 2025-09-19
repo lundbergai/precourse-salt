007d3c2b507cdca2474e0f7ca5fc5fa261596ca2
Viktor Lundberg <vikkolundberg@gmail.com>
2025-09-19 10:58:51 +0200

    implement create new developers functions

--- mvcDotNetTrainingGround/Controllers/DevelopersController.cs
+++ mvcDotNetTrainingGround/Controllers/DevelopersController.cs
@@ -20,4 +20,24 @@ public class DevelopersController : Controller
 		var developer = _db.Developers.Find(d => d.Id == id);
 		return View(developer);
 	}
+
+	public IActionResult Create()
+	{
+		return View();
+	}
+
+	[HttpPost]
+	public IActionResult Create(CreateNewDeveloperViewModel viewModel)
+	{
+		var nextId = _db.Developers.Count + 1;
+		var developerToAdd = new Developer()
+		{
+			Id = nextId,
+			Name = viewModel.Name,
+			Email = viewModel.Email
+		};
+		_db.Developers.Add(developerToAdd);
+
+		return RedirectToAction(nameof(Index));
+	}
 }
