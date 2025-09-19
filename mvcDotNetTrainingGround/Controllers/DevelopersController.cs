572d7042d66c7028f31a90c345b5613b505f61c4
Viktor Lundberg <vikkolundberg@gmail.com>
2025-09-19 10:32:04 +0200

    create and return View

--- mvcDotNetTrainingGround/Controllers/DevelopersController.cs
+++ mvcDotNetTrainingGround/Controllers/DevelopersController.cs
@@ -4,8 +4,14 @@ using mvcDotNetTrainingGround.Models;
 public class DevelopersController : Controller
 {
 	private Db _db;
+
 	public DevelopersController(Db db)
 	{
 		_db = db;
+	}
+
+	public IActionResult Index()
+	{
+		return View(_db.Developers);
 	}
 }
